1e4844d3d78e94aaed9a6188366a357c5447a3e1
Grzegorz Oleksa <grzesieq92@gmail.com>
2023-09-26 20:44:04 +0200

    two new case (peso and chf) to switch added

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ let buttonElement = document.querySelector(".js-button");
 let resultElement = document.querySelector(".js-result");
 
 const exchangeRates = {
-  euro: 4.5,
-  dollar: 4.8,
+  euro: 4.61,
+  dollar: 4.36,
+  peso: 0.25,
+  frank: 4.76,
 };
 
 buttonElement.addEventListener("click", (event) => {
@@ -22,10 +24,16 @@ buttonElement.addEventListener("click", (event) => {
       result = zlotyElement / exchangeRates.dollar;
       resultElement.innerText = `${result.toFixed(2)} USD`;
       break;
+    case "peso":
+      result = zlotyElement / exchangeRates.peso;
+      resultElement.innerText = `${result.toFixed(2)} PESO`;
+      break;
+    case "frank":
+      result = zlotyElement / exchangeRates.frank;
+      resultElement.innerText = `${result.toFixed(2)} CHF`;
+      break;
 
     default:
       resultElement.innerText = "Nieznana waluta";
   }
-
-  resultElement.innerText = `${result.toFixed(2)} zl`;
 });
